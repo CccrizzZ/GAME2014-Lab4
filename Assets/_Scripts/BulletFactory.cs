6cf32b83d5f438d8f33a97be57a9ab06c972b6fe
Christopher Liu <30523986+CccrizzZ@users.noreply.github.com>
2020-10-16 07:44:01 +0800

    ##

---
+++ Assets/_Scripts/BulletFactory.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BulletFactory : MonoBehaviour
+{
+    [Header("BulletTypes")]
+    public GameObject Bullet1;
+    public GameObject Bullet2;
+    public GameObject Bullet3;
+
+    public BulletManager bulletMgr;
+
+    public GameObject createBullet(BulletType type = BulletType.RANDOM)
+    {
+        if (type == BulletType.RANDOM)
+        {
+            var randomBullet = Random.Range(0, 3);
+            type = (BulletType) randomBullet;
+        }
+
+   
+        GameObject tempBullet = null;
+
+        switch (type)
+        {
+            case BulletType.REGULAR:
+                tempBullet = Instantiate(Bullet1);
+                tempBullet.GetComponent<BulletController>().damage = 10;
+                break;
+            case BulletType.FAT:
+                tempBullet = Instantiate(Bullet2);
+                tempBullet.GetComponent<BulletController>().damage = 15;
+                break;
+            case BulletType.PULSING:
+                tempBullet = Instantiate(Bullet3);
+                tempBullet.GetComponent<BulletController>().damage = 20;
+                break;
+        }
+        tempBullet.transform.parent = transform;
+        tempBullet.SetActive(false);
+
+        return tempBullet;
+    }
+}
